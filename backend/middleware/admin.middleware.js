998b6fded064d8af96c4fc28050b45066db54b8f
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-05 21:59:53 +0300

    Add admin middleware and protectRoute middleware

---
+++ backend/middleware/admin.middleware.js
@@ -0,0 +1,13 @@
+export const admin = async (req, res, next) => {
+  try {
+    if (req.user && req.user.role === admin) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized as an admin" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
