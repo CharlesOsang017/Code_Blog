f122bbf8156875e6e7dea090c2e0d42d98f0f955
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-05 15:52:33 +0300

    Create user and hshed the password

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,28 @@
-export const getRequest = async (req, res)=>{
-    res.send("get request")
-    
-}
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+export const getRequest = async (req, res) => {
+  res.send("get request");
+};
+
+// Register User specificall an admin
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // checking for an existing user
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is  already taken" });
+    }
+    // creating a new user
+    // hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await new User({ email, username, password: hashedPassword });
+    await user.save();
+    return res.status(201).json({ message: "user created succeeffully!" });
+  } catch (error) {
+    return res.status(500).json({message: error.message})    
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getRequest } from '../controllers/user.controller.js';
+import {registerUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get("/", getRequest)
+
+router.post("/", registerUser)
 
 
 
