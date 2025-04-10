d21020487a65c1e2e8568fbe15f4a66f7f43f068
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-10 18:41:02 +0300

    Implement password visibility function

--- frontend/src/components/admin/Login.tsx
+++ frontend/src/components/admin/Login.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,17 +32,22 @@ const Login = () => {
         />
 
         <label className='block mb-2 text-sm font-medium'>Password</label>
-        <input
-          type='password'
-          className='w-full mb-6 px-3 py-2 border rounded'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+        <div className='relative mb-6 w-full'>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            className='w-full mb-6 px-3 py-2 border rounded'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div className='absolute right-3 top-1/3 transform -translate-y-1/2 cursor-pointer text-gray-500'  onClick={() => setShowPassword((prev) => !prev)}>
+            {showPassword ? <Eye size={20} /> :  <EyeOff size={20} /> }
+          </div>
+        </div>
 
         <Button className='w-full'>Log In</Button>
         <small>
-          Don\'t have an account? <Link to='/register'>Register</Link>
+          Don't have an account? <Link to='/register'>Register</Link>
         </small>
       </form>
     </div>
--- frontend/src/components/admin/Register.tsx
+++ frontend/src/components/admin/Register.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
 
   const handleRegister = (e: React.FormEvent) => {
     e.preventDefault();
@@ -40,15 +42,23 @@ const Register = () => {
         />
 
         <label className='block mb-2 text-sm font-medium'>Password</label>
-        <input
-          type='password'
-          className='w-full mb-6 px-3 py-2 border rounded'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+        <div className='relative mb-6 w-full'>
+          <input
+            type={showPassword ? "text" : "password"}
+            className='w-full mb-6 px-3 py-2 border rounded'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div
+            className='absolute right-3 top-1/3 transform -translate-y-1/2 cursor-pointer text-gray-500'
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+          </div>
+        </div>
 
-        <Button className="w-full">Register</Button>
+        <Button className='w-full'>Register</Button>
         <small>
           Already have an account? <Link to='/login'>Login</Link>
         </small>
