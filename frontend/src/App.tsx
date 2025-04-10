d14ca508544ff1e5c6a35623322c352353daeb97
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-10 15:05:58 +0300

    Add login and register  routes for admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,22 +5,25 @@ import AddBlog from "./components/admin/AddBlog";
 import BlogList from "./components/admin/BlogList";
 import Subscriptions from "./components/admin/subscriptions";
 import UserDashboard from "./components/user/UserDashboard";
-
+import Login from "./components/admin/Login";
+import Register from "./components/admin/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* User Routes */}
-        <Route path="/" element={<UserLayout />}>
+        <Route path='/' element={<UserLayout />}>
           <Route index element={<UserDashboard />} />
         </Route>
 
         {/* Admin Layout */}
-        <Route path="/admin/" element={<AdminLayout />}>         
-          <Route path="add-blog" element={<AddBlog />} />
-          <Route path="blog-list" element={<BlogList />} />
-          <Route path="subscriptions" element={<Subscriptions />} />
+        <Route path='/admin/' element={<AdminLayout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='add-blog' element={<AddBlog />} />
+          <Route path='blog-list' element={<BlogList />} />
+          <Route path='subscriptions' element={<Subscriptions />} />
         </Route>
       </Routes>
     </Router>
