aaac5c0225446816afcb79d88231132c67db33ea
CharlesOsang017 <charlesosango02@gmail.com>
2025-04-11 21:24:19 +0300

    Add the hero page

--- frontend/src/components/user/Hero.tsx
+++ frontend/src/components/user/Hero.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-medium'>Latest Blogs</h2>
+      <p className='w-[750px] mt-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates non
+        accusamus accusantium eius cupiditate eligendi, magnam fuga fugiat,
+        ipsum natus quam quibusdam necessitatibus nostrum, laborum inventore
+        pariatur enim dolorem ab.
+      </p>
+      <div className='mt-4 mb-4 flex gap-2 items-center'>
+        <input
+          type='text'
+          placeholder='Enter your email'
+          className='border w-[260px] rounded-md  border-gray-400 px-3 py-2 gap-2 shadow-lg'
+        />
+        <Button className='cursor-pointer'>Subscribe</Button>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- frontend/src/components/user/UserDashboard.tsx
+++ frontend/src/components/user/UserDashboard.tsx
@@ -1,8 +1,10 @@
 import Footer from "./Footer"
+import Hero from "./Hero"
 
 const UserDashboard = () => {
   return (
     <div>
+        <Hero />
         <Footer />
     </div>
   )
